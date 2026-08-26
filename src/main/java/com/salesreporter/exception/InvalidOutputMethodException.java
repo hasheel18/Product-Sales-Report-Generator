ec838e8d715e79ee8205b5de7b472002eca35fce
Danuka lakshitha <danukalakshitha99@gmail.com>
2026-08-26 19:59:12 +0530

    Add custom exception classes

---
+++ src/main/java/com/salesreporter/exception/InvalidOutputMethodException.java
@@ -0,0 +1,9 @@
+package com.salesreporter.exception;
+
+
+public class InvalidOutputMethodException extends Exception {
+
+    public InvalidOutputMethodException(String message) {
+        super(message);
+    }
+}
